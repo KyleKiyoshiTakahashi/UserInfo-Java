9828cfc26f36095950ac1c0fd261ca722c98a992
KyleKiyoshiTakahashi <43252585+KyleKiyoshiTakahashi@users.noreply.github.com>
2019-02-17 09:35:22 -0800

    Add files via upload

---
+++ src/com/codingdojo/web/controllers/Home.java
@@ -0,0 +1,60 @@
+package com.codingdojo.web.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+	 
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String favLang = request.getParameter("favLang");
+		String homeTown = request.getParameter("homeTown");
+		
+		out.write("<h1>Welcome "+firstName+" "+lastName+"<h1>");
+		out.write("<h3>Your favorite language is"+favLang+"<h3>");
+		if (homeTown!=null) {
+			out.write("<h3>Your hometwon"+homeTown+"<h3>");
+		} else {
+			out.write("<h3> Hometown is: unknown! </h3>");
+		}
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
